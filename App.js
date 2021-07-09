842f4a13f468101741093065a851d17e1b5b32e2
Darkslayer07 <shubhmittal297@gmail.com>
2021-07-10 03:34:10 +0530

    Trash commit

--- App.js
+++ App.js
@@ -16,8 +16,10 @@ export default function App()
 }
 const styles = StyleSheet.create({ //using StyleSheet will be optimized in future and it also gives validation
   screenView: {                    // overall it is a good practice to use it
-    padding:50
+    padding:50,
+    width: '100%',
+    height: '100%'
   },
-  inputContainer: {flexDirection:"row",justifyContent:"space-between",alignContent:"center"},
+  inputContainer: {flexDirection:"row",justifyContent:"space-between",alignContent:"flex-end"},
   input: {width: '70%',borderColor : 'black',borderWidth:2,paddingBottom:5}
 });
