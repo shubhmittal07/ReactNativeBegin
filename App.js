87eb8be889c65a597873113f17bb0f2dfda1b61d
Darkslayer07 <shubhmittal297@gmail.com>
2021-07-10 03:19:05 +0530

    Added StyleSheet01

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text,View,TextInput,Button} from 'react-native';
+import {Text,View,TextInput,Button,StyleSheet} from 'react-native';
 
 export default function App()
 {
   return (
-    <View style = {{padding :50}}>
+    <View style = {styles.screenView}>
       <View style = {{flexDirection:"row",justifyContent:"space-between",alignContent:"center"}}>
-        <TextInput placeholder = "Enter your next goal"
+        <TextInput placeholder = "        Enter your next goal"
         style = {{width: '70%',borderColor : 'black',borderWidth:2,paddingBottom:5}}/>
         <Button title="ADD" style={{width:'30%'}}/>
       </View>
@@ -14,3 +14,8 @@ export default function App()
 
   );
 }
+const styles = StyleSheet.create({ //using StyleSheet will be optimized in future and it also gives validation
+  screenView: {                    // overall it is a good practice to use it
+    padding:50
+  }
+});
